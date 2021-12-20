9a9e98c0e212cc3b71f9a62a93717f04e06aff6f
jiuxiao(九霄道长) <yinshen@79xj.cn>
2021-12-20 14:58:35 +0800

    修改收货地址

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -17,8 +17,8 @@ class UserAddressesController extends Controller
     public function create() {
         return view('user_addresses.create_and_edit', ['address' => new UserAddress()]);
     }
-    public function store(UserAddressRequest $request)
-    {
+
+    public function store(UserAddressRequest $request) {
         $request->user()->addresses()->create($request->only([
             'province',
             'city',
@@ -32,4 +32,21 @@ class UserAddressesController extends Controller
         return redirect()->route('user_addresses.index');
     }
 
+    public function edit(UserAddress $user_address) {
+        return view('user_addresses.create_and_edit', ['address' => $user_address]);
+    }
+    public function update(UserAddress $user_address, UserAddressRequest $request)
+    {
+        $user_address->update($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+        ]));
+
+        return redirect()->route('user_addresses.index');
+    }
 }
