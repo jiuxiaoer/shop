1d206d8a0f522c2abc287df97851c04aa0a4e8a8
jiuxiao(九霄道长) <yinshen@79xj.cn>
2021-12-22 10:19:36 +0800

    购物车页面

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,14 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\AddCartRequest;
 use App\Models\CartItem;
+use App\Models\ProductSku;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    public function add(AddCartRequest $request)
+    public function index(Request $request) {
+        $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
+
+        return view('cart.index', ['cartItems' => $cartItems]);
+    }
+    public function remove(ProductSku $sku, Request $request)
     {
-        $user   = $request->user();
-        $skuId  = $request->input('sku_id');
+        $request->user()->cartItems()->where('product_sku_id', $sku->id)->delete();
+
+        return [];
+    }
+    public function add(AddCartRequest $request) {
+        $user = $request->user();
+        $skuId = $request->input('sku_id');
         $amount = $request->input('amount');
 
         // 从数据库中查询该商品是否已经在购物车中
