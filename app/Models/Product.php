8f8e1dca54c82145ba4077d5768699839eaef0f7
jiuxiaoer <yinshen@79xj.cn>
2022-01-10 20:41:30 +0800

    商品类目初始化

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -22,7 +22,10 @@ class Product extends Model
     public function skus() {
         return $this->hasMany(ProductSku::class);
     }
-
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
     public function getImageUrlAttribute() {
         // 如果 image 字段本身就已经是完整的 url 就直接返回
         if (Str::startsWith($this->attributes['image'], ['http://', 'https://'])) {
