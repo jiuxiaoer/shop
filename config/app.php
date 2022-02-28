02ba9675655e70ea82b2fc4172410fb727591936
jiuxiaoer <yinshen@79xj.cn>
2022-02-28 17:06:57 +0800

    创建分期付款

--- config/app.php
+++ config/app.php
@@ -42,7 +42,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool)env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -236,4 +236,11 @@ return [
 
     ],
     'ngrok_url' => env('NGROK_URL'),
+    'installment_fee_rate' => [
+        3 => 1.5,
+        6 => 2,
+        12 => 2.5,
+    ], // 分期费率，key 为期数，value 为费率
+    'min_installment_amount' => 300, // 最低分期金额
+    'installment_fine_rate' => 0.05, // 逾期日息 0.05%
 ];
