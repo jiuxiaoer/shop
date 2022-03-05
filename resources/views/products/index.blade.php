41f88257e24ea690da6c8fedf270d2e30bfafbf7
jiuxiaoer <yinshen@79xj.cn>
2022-03-05 20:00:42 +0800

    修复分面搜索 Bug

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -7,49 +7,48 @@
       <div class="card">
         <div class="card-body">
           <!-- 筛选组件开始 -->
-          <form action="{{ route('products.index') }}" class="form-inline search-form">
-            <!-- 创建一个隐藏字段 -->
+          <form action="{{ route('products.index') }}" class="search-form">
             <input type="hidden" name="filters">
             <div class="form-row">
               <div class="col-md-9">
                 <div class="form-row">
-                  <div class="col-auto">
-                    <!-- 面包屑开始 -->
+
+                  <!-- 面包屑开始 -->
+                  <div class="col-auto category-breadcrumb">
                     <!-- 添加一个名为 全部 的链接，直接跳转到商品列表页 -->
-                    <a class="all-products" href="{{ route('products.index') }}">全部</a> &gt;
+                    <a class="all-products" href="{{ route('products.index') }}">全部</a> >
                     <!-- 如果当前是通过类目筛选的 -->
                   @if ($category)
                     <!-- 遍历这个类目的所有祖先类目，我们在模型的访问器中已经排好序，因此可以直接使用 -->
                     @foreach($category->ancestors as $ancestor)
                       <!-- 添加一个名为该祖先类目名的链接 -->
                         <span class="category">
-              <a href="{{ route('products.index', ['category_id' => $ancestor->id]) }}">{{ $ancestor->name }}</a>
-            </span>
-                        <span>></span>
+                  <a href="{{ route('products.index', ['category_id' => $ancestor->id]) }}">{{ $ancestor->name }}</a>
+                </span>
+                        <span>&gt;</span>
                     @endforeach
                     <!-- 最后展示出当前类目名称 -->
                       <span class="category">{{ $category->name }}</span><span> ></span>
                       <!-- 当前类目的 ID，当用户调整排序方式时，可以保证 category_id 参数不丢失 -->
                       <input type="hidden" name="category_id" value="{{ $category->id }}">
                   @endif
+
                   <!-- 商品属性面包屑开始 -->
                     <!-- 遍历当前属性筛选条件 -->
                     @foreach($propertyFilters as $name => $value)
                       <span class="filter">{{ $name }}:
-            <span class="filter-value">{{ $value }}</span>
+                  <span class="filter-value">{{ $value }}</span>
                         <!-- 调用之后定义的 removeFilterFromQuery -->
-            <a class="remove-filter" href="javascript: removeFilterFromQuery('{{ $name }}')">×</a>
-          </span>
+                  <a class="remove-filter" href="javascript: removeFilterFromQuery('{{ $name }}')">×</a>
+                </span>
                   @endforeach
                   <!-- 商品属性面包屑结束 -->
+
+                  </div>
                   <!-- 面包屑结束 -->
-                  </div>
-                  <div class="col-auto">
-                    <input type="text" class="form-control form-control-sm" name="search"
-                           placeholder="搜索"></div>
-                  <div class="col-auto">
-                    <button class="btn btn-primary btn-sm">搜索</button>
-                  </div>
+
+                  <div class="col-auto"><input type="text" class="form-control form-control-sm" name="search" placeholder="搜索"></div>
+                  <div class="col-auto"><button class="btn btn-primary btn-sm">搜索</button></div>
                 </div>
               </div>
               <div class="col-md-3">
@@ -65,14 +64,14 @@
               </div>
             </div>
           </form>
-          <!-- 筛选组件结束 -->
+
           <!-- 展示子类目开始 -->
           <div class="filters">
             <!-- 如果当前是通过类目筛选，并且此类目是一个父类目 -->
             @if ($category && $category->is_directory)
               <div class="row">
-                <div class="col-xs-3 filter-key">子类目：</div>
-                <div class="col-xs-9 filter-values">
+                <div class="col-3 filter-key">子类目：</div>
+                <div class="col-9 filter-values">
                   <!-- 遍历直接子类目 -->
                   @foreach($category->children as $child)
                     <a href="{{ route('products.index', ['category_id' => $child->id]) }}">{{ $child->name }}</a>
@@ -80,32 +79,39 @@
                 </div>
               </div>
             @endif
-          </div>
           <!-- 分面搜索结果开始 -->
-          <!-- 遍历聚合的商品属性 -->
-          @foreach($properties as $property)
-            <div class="row">
-              <div class="col-xs-3 filter-key">{{ $property['key'] }}：</div>
-              <div class="col-xs-9 filter-values">
-              @foreach($property['values'] as $value)
-                <!-- 调用下面定义的 appendFilterToQuery 函数 -->
-                  <a href="javascript: appendFilterToQuery('{{ $property['key'] }}', '{{ $value }}')">{{ $value }}</a>
-                @endforeach
+            <!-- 遍历聚合的商品属性 -->
+            @foreach($properties as $property)
+              <div class="row">
+                <div class="col-3 filter-key">{{ $property['key'] }}：</div>
+                <div class="col-9 filter-values">
+                @foreach($property['values'] as $value)
+                  <!-- 调用下面定义的 appendFilterToQuery 函数 -->
+                    <a href="javascript: appendFilterToQuery('{{ $property['key'] }}', '{{ $value }}')">{{ $value }}</a>
+                  @endforeach
+                </div>
               </div>
-            </div>
-        @endforeach
-        <!-- 分面搜索结果结束 -->
+          @endforeach
+          <!-- 分面搜索结果结束 -->
+
+          </div>
           <!-- 展示子类目结束 -->
+
+          <!-- 筛选组件结束 -->
           <div class="row products-list">
             @foreach($products as $product)
               <div class="col-3 product-item">
                 <div class="product-content">
                   <div class="top">
-                    <div class="img"><a href="{{ route('products.show', ['product' => $product->id]) }}">
+                    <div class="img">
+                      <a href="{{ route('products.show', ['product' => $product->id]) }}">
                         <img src="{{ $product->image_url }}" alt="">
-                      </a></div>
+                      </a>
+                    </div>
                     <div class="price"><b>￥</b>{{ $product->price }}</div>
-                    <div class="title">{{ $product->title }}</div>
+                    <div class="title">
+                      <a href="{{ route('products.show', ['product' => $product->id]) }}">{{ $product->title }}</a>
+                    </div>
                   </div>
                   <div class="bottom">
                     <div class="sold_count">销量 <span>{{ $product->sold_count }}笔</span></div>
@@ -115,21 +121,21 @@
               </div>
             @endforeach
           </div>
+
           <div class="float-right">{{ $products->appends($filters)->render() }}</div>
         </div>
       </div>
     </div>
   </div>
 @endsection
+
+
 @section('scriptsAfterJs')
   <script>
     var filters = {!! json_encode($filters) !!};
     $(document).ready(function () {
-      $('.search-form input[name=search]').val(filters.search);
-      $('.search-form select[name=order]').val(filters.order);
-      $('.search-form select[name=order]').on('change', function () {
-        $('.search-form').submit();
-      });
+
+
       // 之前监听的切换排序方式事件
       $('.search-form select[name=order]').on('change', function() {
         // 解析当前查询参数
@@ -141,7 +147,14 @@
         }
         $('.search-form').submit();
       });
-    });
+
+
+      $('.search-form input[name=search]').val(filters.search);
+      $('.search-form select[name=order]').val(filters.order);
+
+
+    })
+
     // 定义一个函数，用于解析当前 Url 里的参数，并以 Key-Value 对象形式返回
     function parseSearch() {
       // 初始化一个空对象
@@ -213,6 +226,6 @@
       // 重新构建查询参数，并触发浏览器跳转
       location.search = buildSearch(searches);
     }
-  </script>
+
   </script>
 @endsection
